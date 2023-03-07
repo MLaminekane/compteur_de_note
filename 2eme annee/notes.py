dccbb152f3d5c5153137a73e773cbb9cee8e5d0a
Lamine Kane <mlaminekane01@gmail.com>
2023-03-07 13:53:39 +0000

    last modification

--- 2eme annee/notes.py
+++ 2eme annee/notes.py
@@ -23,6 +23,9 @@ bus_engl_exam = float(input("Note anglais exam: "))
 elc_digit_dev = float(input("Note devoir Electrronique digitale: "))
 elc_digit_exam = float(input("Note exam Electrronique digitale: "))
 
+byb_dev = float(input("Note BYB devoir: "))
+byb_exam = float(input("Note BYB exam: "))
+
 langage_C_dev = float(input("Note devoir Langage C: "))
 langage_C_exam = float(input("Note examen Langage C: "))
 
@@ -43,13 +46,14 @@ uml = (uml_dev * 0.4 + algo_dev * 0.6) * coef_2
 cisco = (ccna_dev * 0.4 + ccna_exam * 0.6) * coef_2
 anglais = (bus_engl_dev * 0.4 + bus_engl_exam * 0.6) * coef_2
 elec_dig = (elc_digit_dev* 0.4 + elc_digit_exam * 0.6) * coef_1
+byb = (byb_dev* 0.4 + byb_exam * 0.6) * coef_2
 L_c = (langage_C_dev * 0.4 + langage_C_exam * 0.6) * coef_2
 python = (python_dev * 0.4 + python_exam * 0.6) * coef_2
 php = (php_dev * 0.4 + php_exam * 0.6) * coef_1
 sgbd = (sgbd_dev * 0.4 + sgbd_exam * 0.6) * coef_2
 
 
-moyenne = windows + algo + maths + uml + cisco + anglais + elec_dig + L_c + python + php + sgbd   
+moyenne = windows + algo + maths + uml + cisco + anglais + elec_dig + byb + L_c + python + php + sgbd   
 
 print("Resultat: \n ") 
-print(f"moyenne: {moyenne}; {moyenne / 19}")
+print(f"moyenne: {moyenne}; {moyenne / 21}")
